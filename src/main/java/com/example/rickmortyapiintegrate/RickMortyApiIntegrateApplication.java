fcddaa7bd674d861e2e001adb657e87a2053ded3
Mimoona <memoona.raheel8@gmail.com>
2025-04-24 10:53:50 +0200

    initial commit

---
+++ src/main/java/com/example/rickmortyapiintegrate/RickMortyApiIntegrateApplication.java
@@ -0,0 +1,13 @@
+package com.example.rickmortyapiintegrate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RickMortyApiIntegrateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RickMortyApiIntegrateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rickmortyapiintegrate/RickMortyApiIntegrateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rickmortyapiintegrate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RickMortyApiIntegrateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
